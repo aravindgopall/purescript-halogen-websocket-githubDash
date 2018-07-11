09ad12ba71a073e9c33eb93d63c5bb330e0e674c
aravind.mallapureddy <aravind.mallapureddy@juspay.in>
2018-07-11 13:56:14 +0530

    bower components

---
+++ bower_components/purescript-foreign-generic/test/Main.purs
@@ -0,0 +1,54 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Except (runExcept)
+import Data.Bifunctor (bimap)
+import Data.Either (Either(..))
+import Data.Foreign.Class (class Encode, class Decode)
+import Data.Foreign.Generic (decodeJSON, encodeJSON)
+import Data.Foreign.NullOrUndefined (NullOrUndefined(..))
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Test.Assert (assert, assert', ASSERT)
+import Test.Types (IntList(..), RecordTest(..), Tree(..), TupleArray(..), UndefinedTest(..))
+
+buildTree :: forall a. (a -> TupleArray a a) -> Int -> a -> Tree a
+buildTree _ 0 a = Leaf a
+buildTree f n a = Branch $ buildTree (bimap f f) (n - 1) (f a)
+
+-- A balanced binary tree of depth N
+makeTree :: Int -> Tree Int
+makeTree n = buildTree (\i -> TupleArray (Tuple (2 * i) (2 * i + 1))) n 0
+
+throw :: forall eff. String -> Eff (assert :: ASSERT | eff) Unit
+throw = flip assert' false
+
+testRoundTrip
+  :: ∀ a eff
+   . Eq a
+  => Decode a
+  => Encode a
+  => a
+  -> Eff ( console :: CONSOLE
+         , assert :: ASSERT
+         | eff
+         ) Unit
+testRoundTrip x = do
+  let json = encodeJSON x
+  log json
+  case runExcept (decodeJSON json) of
+    Right y -> assert (x == y)
+    Left err -> throw (show err)
+
+main :: forall eff. Eff (console :: CONSOLE, assert :: ASSERT | eff) Unit
+main = do
+  testRoundTrip (RecordTest { foo: 1, bar: "test", baz: 'a' })
+  testRoundTrip (Cons 1 (Cons 2 (Cons 3 Nil)))
+  testRoundTrip (UndefinedTest {a: NullOrUndefined (Just "test")})
+  testRoundTrip (UndefinedTest {a: NullOrUndefined Nothing})
+  testRoundTrip [NullOrUndefined (Just "test")]
+  testRoundTrip [NullOrUndefined (Nothing :: Maybe String)]
+  testRoundTrip (makeTree 0)
+  testRoundTrip (makeTree 5)
---
+++ bower_components/purescript-halogen/src/Halogen/Aff/Effects.purs
@@ -0,0 +1,18 @@
+module Halogen.Aff.Effects where
+
+import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Now (NOW)
+import Control.Monad.Eff.Ref (REF)
+import DOM (DOM)
+
+-- | A type alias for the basic row of effects involved in running Halogen with
+-- | `Aff`-based drivers.
+type HalogenEffects eff =
+  ( avar :: AVAR
+  , ref :: REF
+  , exception :: EXCEPTION
+  , dom :: DOM
+  , now :: NOW
+  | eff
+  )
